9b15c9f74a0acbb970b4fa6694b2f2f3291d6010
unknown <rios_2094@hotmail.com>
2025-07-24 22:00:14 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,52 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Conversor from './Conversor'
+
+/*Creacion de constantes para almacenar valores*/
+function App() {
+  const [usuario, setUsuario] = useState('')
+  const [clave, setClave] = useState('')
+  const [logueado, setLogueado] = useState(false)
+
+  //Actualiza el estado con el valor del campo de usuario
+  function cambiarUsuario(evento) {
+    setUsuario(evento.target.value)
+  }
+
+  //Actualiza el estado con el valor del campo de contraseña
+  function cambiarClave(evento) {
+    setClave(evento.target.value)
+  }
+
+  //Se verifica que las credenciales sean "admin" / "admin"
+  function ingresar() {
+    if (usuario == 'admin' && clave == 'admin') {
+      alert('Ingresaste')
+      setLogueado(true)
+    } else {
+      alert('Usuario o clave incorrectos')
+    }
+  }
+
+//Si se cumple el inicio de sesion se muestra el componente conversor
+  if (logueado){
+    return <Conversor/>
+
+  }
+
+  return (
+    //Nos muestra los campos para usuario y contraseña
+    <>
+    <h1>Inicio de sesion</h1>
+        <input placeholder='Usuario' type="text" name="usuario" id="usuario" value={usuario} onChange={cambiarUsuario} />
+        <input placeholder='Clave' type="password" name="clave" id="clave" value={clave} onChange={cambiarClave} />
+        <button onClick={ingresar}>Ingresar</button>
+    </>
+
+  );
+
+  }
+
+export default App
